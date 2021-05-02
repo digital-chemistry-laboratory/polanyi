c49bee789b3195b98676277d39de207efa4d99e8
Kjell Jorner <kjell.jorner@gmail.com>
2021-05-02 22:11:23 +0100

    Initial commit

---
+++ polanyi/__init__.py
@@ -0,0 +1,6 @@
+"""Calculates transition states."""
+
+from importlib import metadata
+
+# Version of the polanyi package
+__version__ = metadata.version("polanyi")
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+URL = ""
+DESCRIPTION = "Transition state optimizations made easy."
+LONG_DESCRIPTION = f"""\
+{DESCRIPTION}. For more information, see the [project repository]({URL}).
+"""
+
+setuptools.setup(
+    name="polanyi",
+    version="0.0.1",
+    author="Kjell Jorner",
+    author_email="kjell.jorner@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=["polanyi"],
+    python_requires=">=3.8",
+    install_requires=["numpy"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
